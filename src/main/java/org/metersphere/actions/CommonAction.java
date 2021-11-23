d830616b479dffb740a684aa1f7e32a26efa6d2b
zhangdahai112 <dahai.zhang@fit2cloud.com>
2021-11-23 14:15:38 +0800

    fix:修复加入进度条后导出失败还是显示成功的问题

--- src/main/java/org/metersphere/actions/CommonAction.java
+++ src/main/java/org/metersphere/actions/CommonAction.java
@@ -6,7 +6,10 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.util.ThrowableComputable;
 import org.metersphere.exporter.ExporterFactory;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public abstract class CommonAction extends AnAction {
     protected void export(String source, AnActionEvent event) {
@@ -14,20 +17,21 @@ public abstract class CommonAction extends AnAction {
         if (project == null) {
             return;
         }
-        boolean r;
-        try {
-            r = ProgressManager.getInstance().runProcessWithProgressSynchronously(() -> {
-                ApplicationManager.getApplication().runReadAction(() -> {
-                    ProgressManager.getGlobalProgressIndicator().setText("begin...");
+        AtomicBoolean r = new AtomicBoolean(false);
+        ProgressManager.getInstance().runProcessWithProgressSynchronously(() -> {
+            try {
+                ApplicationManager.getApplication().runReadAction((ThrowableComputable<Boolean, Throwable>) () -> {
+                    ProgressManager.getGlobalProgressIndicator().setText("begin exporting...");
                     if (!ExporterFactory.export(source, event))
                         throw new RuntimeException("failed");
+                    return true;
                 });
-            }, "Exporting api to MeterSphere please wait...", true, event.getProject());
-        } catch (Exception e) {
-            r = false;
-        }
+            } catch (Throwable throwable) {
+                r.set(false);
+            }
+        }, "Exporting api to MeterSphere please wait...", true, event.getProject());
 
-        if (r)
+        if (r.get())
             Messages.showInfoMessage("Export to MeterSphere success!", "Info");
         else
             Messages.showInfoMessage("Export to MeterSphere fail! please see log file in idea.log", "Info");
