82f40d4757f831553caff5115776f485aa0db4e7
fit2-zhao <yong.zhao@fit2cloud.com>
2024-07-13 15:08:51 +0800

    chore: 版本兼容调整

--- src/main/java/io/metersphere/icons/MsIcons.java
+++ src/main/java/io/metersphere/icons/MsIcons.java
@@ -5,5 +5,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public class MsIcons {
-    public static Icon Icon = IconLoader.getIcon("icons/logo.jpg", MsIcons.class);
+    public static Icon Icon = IconLoader.getIcon("icons/icon.png", MsIcons.class);
 }
